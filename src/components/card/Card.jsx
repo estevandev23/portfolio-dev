2cec177e660694d51f2d6a4519171c281763dc2d
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-04-18 10:26:16 -0500

    Add styles to backdrop

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion'
 import './card.css'
-import { GitHubIcon, WebIcon } from '../Icons'
 import { useLayoutEffect, useRef, useState } from 'react'
+import LinksUrlIcons from '../linksUrlIcons/LinksUrlIcons'
 
 const childrens = {
   hidden: { opacity: 0, x: -10 },
@@ -13,8 +13,8 @@ const childrens = {
 
 function mouseMove (e, height, width) {
   const { layerX, layerY } = e
-  const x = ((layerX - width / 2) / width) * 10
-  const y = ((layerY - height / 2) / height) * 10
+  const x = ((layerX - width / 2) / width) * 5
+  const y = ((layerY - height / 2) / height) * 5
 
   return `perspective(500px) rotateX(${x}deg) rotateY(${y}deg) scale(1.05)`
 }
@@ -25,10 +25,24 @@ function resetStyles () {
 
 export default function Card ({ id, name, description, url, image, tags, date, setIdLayout, item, element }) {
   const ref = useRef(null)
+  const refMouse = useRef(null)
+
   const [styles3D, setStyles3D] = useState(resetStyles())
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  function ligthMouseMove (event) {
+    const { layerX, layerY } = event.nativeEvent
+    setPosition({ x: layerX, y: layerY })
+  }
+
+  function removeEvent () {
+    setPosition({ x: 0, y: 0 })
+  }
 
   useLayoutEffect(() => {
     ref.current.addEventListener('mousemove', (event) => setStyles3D(mouseMove(event, ref.current.clientHeight, ref.current.clientWidth)))
+
+    return ref.current.removeEventListener('mousemove', (event) => setStyles3D(mouseMove(event, ref.current.clientHeight, ref.current.clientWidth)))
   }, [])
 
   return (
@@ -36,8 +50,13 @@ export default function Card ({ id, name, description, url, image, tags, date, s
       style={{ transform: styles3D }}
       className='container-card'
       ref={ref}
-      onMouseLeave={() => setStyles3D(resetStyles())}
+      onMouseMove={(event) => ligthMouseMove(event)}
+      onMouseLeave={() => {
+        setStyles3D(resetStyles())
+        removeEvent()
+      }}
     >
+      <div ref={refMouse} className='circle-ligth' style={{ transform: `translate(${position.x}px, ${position.y}px)` }} />
       <motion.div
         variants={item}
         layoutId={id}
@@ -48,8 +67,7 @@ export default function Card ({ id, name, description, url, image, tags, date, s
         <motion.img src={image[0]} />
         <motion.p variants={childrens}>{description.substring(0, 50)}...</motion.p>
         <motion.div className='card-urls' variants={childrens}>
-          <motion.a href={url.public} className='links-project' target='_blank'><WebIcon />View site</motion.a>
-          <motion.a href={url.github} className='links-project' target='_blank'><GitHubIcon />View code</motion.a>
+          <LinksUrlIcons url={url} />
         </motion.div>
         <motion.div variants={childrens} className='content-card-time'>
           <p className='date-card'>{date}</p>
---
+++ src/components/linksUrlIcons/LinksUrlIcons.jsx
@@ -0,0 +1,11 @@
+import { motion } from 'framer-motion'
+import { GitHubIcon, WebIcon } from '../Icons'
+
+export default function LinksUrlIcons ({ url }) {
+  return (
+    <>
+      <motion.a href={url.public} className='links-project' target='_blank'><WebIcon />View site</motion.a>
+      <motion.a href={url.github} className='links-project' target='_blank'><GitHubIcon />View code</motion.a>
+    </>
+  )
+}
